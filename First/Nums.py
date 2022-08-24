00ea8074ef5f1fa1b5e4e944c2a31b7594036c9d
Ba6aka <109065610+Ba6aka@users.noreply.github.com>
2022-08-24 11:27:03 +0300

    add new function split

--- First/Nums.py
+++ First/Nums.py
@@ -2,5 +2,13 @@ def plus(a, b):
     return a + b
 
 
+text = "sgasdgasg"
+
+
+def split(text):
+    return text.split("a")
+
+
 if __name__ == "__main__":
     print(plus(2, 2))
+    print(split(text))
